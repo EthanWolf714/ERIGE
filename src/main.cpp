c1ef46dbac0bc449fe07d0245b9382873f98c469
Ethan Wolf <Emwolf16@yahoo.com>
2026-03-20 01:38:38 -0500

    started main raycast loop, calculating direction and postion as well as initialize global variables

--- src/main.cpp
+++ src/main.cpp
@@ -1,30 +1,78 @@
 #include "raylib.h"
 
+#define mapWidth 24
+#define mapHeight 24
+#define screenWidth 800
+#define screenHeight 450
+
 //------------------------------------------------------------------------------------
 // Program main entry point
 //------------------------------------------------------------------------------------
+int worldMap[mapWidth][mapHeight] =
+{
+    {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,0,0,0,0,0,2,2,2,2,2,0,0,0,0,3,0,3,0,3,0,0,0,1},
+  {1,0,0,0,0,0,2,0,0,0,2,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,0,0,0,0,0,2,0,0,0,2,0,0,0,0,3,0,0,0,3,0,0,0,1},
+  {1,0,0,0,0,0,2,0,0,0,2,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,0,0,0,0,0,2,2,0,2,2,0,0,0,0,3,0,3,0,3,0,0,0,1},
+  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,4,4,4,4,4,4,4,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,4,0,4,0,0,0,0,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,4,0,0,0,0,5,0,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,4,0,4,0,0,0,0,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,4,0,4,4,4,4,4,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,4,4,4,4,4,4,4,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1}
+};
+
+
+double posX = 22, posY = 12;
+double dirX = -1, dirY = 0;
+double planeX = 0, planeY = 0.66;
+
 int main()
 {
+
     // Initialization
-   
-    const int screenWidth = 800;
-    const int screenHeight = 450;
-
-    InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
-
     
+    InitWindow(screenWidth, screenHeight, "Ulfr Engine (raycaster)");
+
+   
     SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
     
 
     // Main game loop
     while (!WindowShouldClose())    // Detect window close button or ESC key
     {
+        
         // Update
     
         // Draw
        
         BeginDrawing();
-            ClearBackground(BLACK);
+            for(int x = 0; x < screenWidth; x++){
+                //calculate ray position and direction
+                double cameraX = 2 * x / double(screenWidth) - 1; //x-coordinate in camera space
+                double rayDirX = dirX + planeX * cameraX;
+                double rayDirY = dirY + planeY * cameraX;
+
+                //which box of the map we're in
+                int mapX = int(posX);
+                int mapY = int(posY);
+
+                //length of ray from current postiions to next x or y-side
+            }
             
 
             //DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
